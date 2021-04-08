8f4fb19b689f284ff9d22bd3e4d1c97b7f4f20cc
Jean Paulo Silva Vasconcelos <jean.vasconcelos6@gmail.com>
2021-04-08 12:33:19 -0300

    Initial commit

---
+++ src/zoo.js
@@ -0,0 +1,80 @@
+/*
+eslint no-unused-vars: [
+  "error",
+  {
+    "args": "none",
+    "vars": "local",
+    "varsIgnorePattern": "data"
+  }
+]
+*/
+
+const data = require('./data');
+
+function animalsByIds(ids) {
+  // seu código aqui
+}
+
+function animalsOlderThan(animal, age) {
+  // seu código aqui
+}
+
+function employeeByName(employeeName) {
+  // seu código aqui
+}
+
+function createEmployee(personalInfo, associatedWith) {
+  // seu código aqui
+}
+
+function isManager(id) {
+  // seu código aqui
+}
+
+function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+  // seu código aqui
+}
+
+function animalCount(species) {
+  // seu código aqui
+}
+
+function entryCalculator(entrants) {
+  // seu código aqui
+}
+
+function animalMap(options) {
+  // seu código aqui
+}
+
+function schedule(dayName) {
+  // seu código aqui
+}
+
+function oldestFromFirstSpecies(id) {
+  // seu código aqui
+}
+
+function increasePrices(percentage) {
+  // seu código aqui
+}
+
+function employeeCoverage(idOrName) {
+  // seu código aqui
+}
+
+module.exports = {
+  entryCalculator,
+  schedule,
+  animalCount,
+  animalMap,
+  animalsByIds,
+  employeeByName,
+  employeeCoverage,
+  addEmployee,
+  isManager,
+  animalsOlderThan,
+  oldestFromFirstSpecies,
+  increasePrices,
+  createEmployee,
+};
