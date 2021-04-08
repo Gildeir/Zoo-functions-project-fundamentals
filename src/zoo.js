2b8b74bc01a81591eec33e619e5e0d2c315cf9a6
Gildeir <gildeirlopes@gmail.com>
2021-04-08 14:51:56 -0300

    Commit inicial

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(ids) {
-  // seu código aqui
+  // seu código aqui,
 }
 
 function animalsOlderThan(animal, age) {
