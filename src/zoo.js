cb29e294385913211b9fc88a1ec93b2011d0696f
Gildeir <gildeirlopes@gmail.com>
2021-04-10 18:00:08 -0300

    Requisito addEmplyee

--- src/zoo.js
+++ src/zoo.js
@@ -35,9 +35,20 @@ function isManager(ids) {
   return employees.some((manager) => manager.managers.includes(ids));
 }
 
-// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-
-// }
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const obj = [{ id, firstName, lastName, managers, responsibleFor }];
+  obj.forEach((e) => employees.push(e));
+  // console.log(employees);
+}
+// addEmployee('4141da1c-a6ed-4cf7-90c4-99c657ba4ef3', 'Jane', 'Doe',
+//   [
+//     '546fe3d4-2d81-4bb4-83a7-92d5b7048d17',
+//     'a67a36ee-3765-4c74-8e0f-13f881f6588a',
+//   ],
+//   [
+//     'ee6139bf-b526-4653-9e1e-1ca128d0ad2e',
+//     '210fcd23-aa7b-4975-91b7-0230ebb27b99',
+//   ]);
 
 function animalCount(species) {
   const empty = {};
@@ -80,7 +91,7 @@ module.exports = {
   animalsByIds,
   employeeByName,
   // employeeCoverage,
-  // addEmployee,
+  addEmployee,
   isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
